555e8669c8fa5f87a7db0312eb68c11e2165b975
Muxammadali <115696592+Mukhammaddaali@users.noreply.github.com>
2023-02-07 03:08:17 +0500

    done

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,8 +1,17 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class NumberDivider {
     void divide(int toBeDivided) {
         float division;
         division=toBeDivided/5;
+        System.out.println(division);
+    }
+    public void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int a;
+        a= input.nextInt();
+        divide(a);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,9 +1,16 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class TimeConvertor {
     public void convert(float minutes) {
         int second;
-        second = minutes*60;
+        second = (int) (minutes*60);
         System.out.println(second);
     }
-}
+    public void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int a;
+        a= input.nextInt();
+        convert(a);
+}}
